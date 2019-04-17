174959abae821c8e7aa951005d6f48a83d9a44bc
Harry Westbrook <up785080@myport.ac.uk>
2019-04-17 15:14:38 +0100

    Scripts and Styling.
    Styling updates for front-end and changes to API to clean up appearance and speed.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
+function jsUcfirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const app = document.getElementById('root')
 
-// const logo = document.createElement('img')
-// logo.src = 'logo.png'
+const logo = document.createElement('img')
+logo.setAttribute('class', 'logo')
+logo.src = 'logo.png'
+
+const desc = document.createElement('h2')
+desc.textContent = 'A project by Harry to try and learn more about reactive web apps using restful apis'
 
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
-// app.appendChild(logo)
+app.appendChild(logo)
+app.appendChild(desc)
 app.appendChild(container)
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://pokeapi.co/api/v2/pokemon', true)
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon?limit=150', true)
 
 request.onload = function () {
     var data = JSON.parse(this.response)
@@ -26,40 +35,42 @@ request.onload = function () {
             card.setAttribute('class', 'card')
 
             const h1 = document.createElement('h1')
-            h1.textContent = pokemon.name
+            h1.textContent = jsUcfirst(pokemon.name)
 
             container.appendChild(card)
 
             card.appendChild(h1)
-            // console.log(pokemon.name)
-            // console.log(pokemon.url)
 
             var detailrequest = new XMLHttpRequest()
 
             detailrequest.open('GET', pokemon.url, true)
-
+    
             detailrequest.onload = function(){
+
                 var detaildata = JSON.parse(this.response)
+
+                const img = document.createElement('img')
+                
+                img.setAttribute('class', 'pokemon_image')
+                img.src = detaildata.sprites.front_default
+
+                card.appendChild(img)
 
                 for (let i = 0; i < detaildata.types.length; i++) {
                     const p = document.createElement('p')
 
                     p.setAttribute('class', 'type' + i)
-                    p.textContent = 'Type ' + i + ': ' + detaildata.types[i].type.name
+                    p.textContent = 'Type ' + i + ': ' + jsUcfirst(detaildata.types[i].type.name)
 
                     card.appendChild(p)
                 }
-                console.log(detaildata.types[0].type.name)
-                // console.log(detaildata.base_experience)
-                // detaildata.types.foreach(pokedetail => {
-                //     // console.log(pokedetail.type.name)
-                // })
-   
             }
             detailrequest.send()
         })
     } else {
-        console.log('this aint it son')
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = `Gah, it's not working!`
+        app.appendChild(errorMessage)
     }
 }
 
